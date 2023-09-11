5267e3eb68ca33aa90da9cf05949bc7aefb2b413
student <student@gnu.ac.in>
2023-09-11 10:36:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_7_20012011082/youtube.kt
@@ -0,0 +1,11 @@
+package com.example.mad_practical_7_20012011082
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class youtube : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+    }
+}
